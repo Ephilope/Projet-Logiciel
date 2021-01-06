77e0a5e36717c5005f27ed8b0c3b896d26b149c2
Ephilope <benoit.decachard@wanadoo.fr>
2021-01-06 12:55:03 +0100

    define macro registre

--- arm_simulator-1.4/registers.c
+++ arm_simulator-1.4/registers.c
@@ -25,13 +25,51 @@ Contact: Guillaume.Huard@imag.fr
 #include <stdlib.h>
 #include <stdio.h>
 
+#define R0 0 
+#define R1 1 
+#define R2 2 
+#define R3 3 
+#define R4 4 
+#define R5 5 
+#define R6 6 
+#define R7 7 
+#define R8 8
+#define R9 9
+#define R10 10
+#define R11 11
+#define R12 12
+#define R13 13
+#define R14 14
+#define PC 15
+#define CPSR 16
+#define R13_svc 17
+#define R14_svc 18
+#define SPSR_svc 19
+#define R13_abt 20
+#define R14_abt 21
+#define SPSR_abt 22
+#define R13_und 23
+#define R14_und 24
+#define SPSR_und 25
+#define R13_irq 26
+#define R14_irq 27
+#define SPSR_irq 28
+#define R8_fiq 29
+#define R9_fiq 30
+#define R10_fiq 31
+#define R11_fiq 32
+#define R12_fiq 33
+#define R13_fiq 34
+#define R14_fiq 35
+#define SPSR_fiq 36
 
 
 
 
 
 struct registers_data {
-    uint32_t  regist_array[22];
+    uint32_t regs[37];
+    uint8_t mode;
 
 };
 
