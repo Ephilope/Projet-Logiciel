a35fa8acb6d1711297830c688880622bb52c0aaa
scartelo <scartelo@icloud.com>
2021-01-09 07:28:39 +0100

    Etape 2 : Décodage et exécution et commande stepi finie

--- arm_simulator-1.4/arm_instruction.c
+++ arm_simulator-1.4/arm_instruction.c
@@ -29,20 +29,85 @@ Contact: Guillaume.Huard@imag.fr
 #include "util.h"
 
 
-int condition_OK(uint8_t condition){
-    return 1;
+int condition_OK(uint8_t condition, uint8_t flag_N, uint8_t flag_Z, uint8_t flag_C, uint8_t flag_V){
+    int result = 0;
+    switch (condition){
+    case EQ:        // Equal : Z set
+        result = (flag_Z == 1);
+        break;		
+    case NE:        // Not equal : Z clear
+        result = (flag_Z == 0);
+        break;
+    case CS_HS:     // Carry set/unsigned higher or same : C set
+        result = ( flag_C == 1);
+        break;
+    case CC_LO: 	// Carry clear/unsigned lower : C clear
+        result = ( flag_C == 0);
+        break;
+    case MI:		// Minus/negative : N set
+        result = ( flag_N == 1);
+    case PL:		// Plus/positive or zero : N clear
+        result = ( flag_N == 0 );
+        break;
+    case VS:		// Overflow V set
+        result = ( flag_V == 1);
+        break;
+    case VC:		// No overflow : V clear
+        result = ( flag_V == 0);
+        break;
+    case HI: 		// Unsigned higher : C set and Z clear
+        result = ( flag_C == 1 && flag_Z == 0);
+        break;
+    case LS:		// Unsigned lower or same : C clear or Z set
+        result = ( flag_C == 0 && flag_Z == 1);
+        break;
+    case GE:		// Signed greater than or equal : N set and V set, or N clear and V clear (N == V)
+        result = ( flag_N == flag_V);
+        break;
+    case LT:		// Signed less than : N set and V clear, or N clear and V set (N != V)
+        result = ( flag_N != flag_V);
+        break;
+    case GT:  		// Signed greater than : Z clear, and either N set and V set, or N clear and V clear (Z == 0 and N == V)
+        result = ( flag_Z == 0 && flag_N == flag_V);
+        break;
+    case LE:		// Signed less than or equal : Z set, or N set and V clear, or N clear and V set (Z == 1 or N != V)
+        result = ( flag_Z == 1 && flag_N != flag_V);
+        break;
+    case AL:  		// Always (unconditional)
+        result = 1;
+        break;
+    case UX:        // Unconditional extension : Condition code 0b1111
+        result = 1;		
+    default:
+        result = 0;
+        fprintf(stderr, "Code condition instruction inconnu [31-28]\n");
+        break;
+    }
+    return result;
 }
 
 int arm_decode(arm_core p){
     uint32_t instruction;
     arm_fetch(p,&instruction);
     int (*fct_decode)(arm_core, uint32_t);
-    int resultat = 0;
+    int result = 0;
+
+    uint8_t two_first_bits_opcode = 0;
+    uint8_t S = 0;
+    uint8_t bit4 = 0;
+    uint8_t bit7 = 0;
+    
     /** Condition of execution of the instruction 
      * See ARM Manual reference A1-6 section A1.2 paragraphe 3 (p.32)
     **/
-    uint8_t cond = (uint8_t) ((instruction >> 28) & 0xF);
-    if (!condition_OK(cond)){
+    uint8_t cond = (uint8_t) get_bits(instruction, 31,28);
+    uint32_t cpsr = arm_read_cpsr(p);
+    uint8_t flag_N = (u_int8_t) get_bit(cpsr, N);
+    uint8_t flag_Z = (u_int8_t) get_bit(cpsr, Z);
+    uint8_t flag_C = (u_int8_t) get_bit(cpsr, C);
+    uint8_t flag_V = (u_int8_t) get_bit(cpsr, V);
+
+    if (!condition_OK(cond, flag_N, flag_Z, flag_C, flag_V)){
         return 1;
     }
     // ge bit fields in the interval [27-25]
@@ -50,44 +115,64 @@ int arm_decode(arm_core p){
     switch (code_instruction){
     // data processing instruction : code_instruction 000 or 001
     case 0x0:
-        fct_decode = arm_data_processing_shift;
-        resultat = fct_decode(p, instruction);
+        two_first_bits_opcode = (uint8_t) get_bits(instruction, 24, 23);
+        S = (uint8_t) get_bit(instruction, 20);
+        bit4 = (uint8_t) get_bit(instruction, 4);
+        bit7 = (uint8_t) get_bit(instruction, 7);
+        if ( (two_first_bits_opcode & 0xF) == 0x2 && ( S & 0xF) == 0x0) {
+            if ( (bit4 & 0xF) == 0x0 || ( bit7 & 0xF) == 0x0){
+                fct_decode = arm_miscellaneous;
+                result = fct_decode(p, instruction);
+            }
+        }else{
+            //fprintf(stderr, "Data process\n");
+            fct_decode = arm_data_processing_shift;
+            result = fct_decode(p, instruction);
+        }
         break;
     case 0x1:
         fct_decode = arm_data_processing_immediate_msr;
-        resultat = fct_decode(p, instruction);
+        result = fct_decode(p, instruction);
         break;
     // Simple memory access, load/ strore : code instruction 010 or 011
     case 0x2:
     case 0x3:
         fct_decode = arm_load_store;
-        resultat =  fct_decode(p, instruction);
+        result =  fct_decode(p, instruction);
         break;
     // Multiple memory access :  code_instruction 100
     case 0x4:
         // LDM / STM
         fct_decode = arm_load_store_multiple;
-        resultat = fct_decode(p, instruction);
+        result = fct_decode(p, instruction);
         break;
     // Branch or other : code_instruction 101
     case 0x5:
         // branch
         fct_decode = arm_branch;
-        resultat = fct_decode(p, instruction);
+        result = fct_decode(p, instruction);
         break;
     case 0x6:
+        fct_decode = arm_coprocessor_load_store;
+        result = fct_decode(p, instruction);
+        break;
     case 0x7:
+        fct_decode = arm_coprocessor_others_swi;
+        result = fct_decode(p, instruction);
         break;
     default:
         fprintf(stderr, "Instruction non prise en compte\n");
-        resultat = UNDEFINED_INSTRUCTION;
+        result = UNDEFINED_INSTRUCTION;
         break;
     }
-    return resultat;
+    return result;
 }
+
 static int arm_execute_instruction(arm_core p) {
-   
-    return 0;
+    int result = arm_decode(p);
+    if (result)
+        arm_exception(p, result);
+    return result;
 }
 
 int arm_step(arm_core p) {
--- arm_simulator-1.4/arm_instruction.h
+++ arm_simulator-1.4/arm_instruction.h
@@ -1,28 +1,45 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #ifndef __ARM_INSTRUCTION_H__
 #define __ARM_INSTRUCTION_H__
 #include "arm_core.h"
+
+#define EQ 0 		// 0000 EQ Equal Z set
+#define NE 1 		// 0001 NE Not equal Z clear
+#define CS_HS 2 	// 0010 CS/HS Carry set/unsigned higher or same C set
+#define CC_LO 3 	// 0011 CC/LO Carry clear/unsigned lower C clear
+#define MI 4		// 0100 MI Minus/negative N set
+#define PL 5		// 0101 PL Plus/positive or zero N clear
+#define VS 6		// 0110 VS Overflow V set
+#define VC 7		// 0111 VC No overflow V clear
+#define HI 8		// 1000 HI Unsigned higher C set and Z clear
+#define LS 9		// 1001 LS Unsigned lower or same C clear or Z set
+#define GE 10		// 1010 GE Signed greater than or equal N set and V set, or N clear and V clear (N == V)
+#define LT 11		// 1011 LT Signed less than N set and V clear, or N clear and V set (N != V)
+#define GT 12 		// 1100 GT Signed greater than Z clear, and either N set and V set, or N clear and V clear (Z == 0,N == V)
+#define LE 13		// 1101 LE Signed less than or equal Z set, or N set and V clear, or N clear and V set (Z == 1 or N != V)
+#define AL 14 		// 1110 AL Always (unconditional) -
+#define UX 15		// 1111 See Condition code 0b1111
 
 int arm_step(arm_core p);
 
