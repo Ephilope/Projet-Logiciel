fe0f50ea4d7010a5d157b366bde3156922e6fe6f
scartelo <scartelo@icloud.com>
2021-01-14 10:22:00 +0100

    Examples data processing

--- arm_simulator-1.4/Examples/example_mov_adds.s
+++ arm_simulator-1.4/Examples/example_mov_adds.s
@@ -4,8 +4,8 @@
 main:
     mov r0, #0x12
     adds r1 , r1 , r2
-    mov r2, #0x512
-    mov r3, #0x500
+    mov r2, #0x80
+    mov r3, #0x88
     subs r2 , r2 , r3
     andeq r2 , r2 , r0 , lsl #7
     swi 0x123456
